5e35b97e8fc85ac6c419dba56c014bce5d2b5ecb
FrzMtrsprt <alex20020703@163.com>
2023-05-16 12:40:06 +0800

    Update DetectionDataset to use generated proposals

--- dataset_models.py
+++ dataset_models.py
@@ -1,6 +1,7 @@
 import os
-from typing import Callable, Dict, List, Optional, Tuple
+from typing import Any, Callable, Dict, List, Optional, Tuple
 
+import pandas as pd
 from PIL import Image
 from torch import Tensor
 from torch.utils.data import Dataset
@@ -60,57 +61,44 @@ class DetectionDataset(Dataset[Tensor]):
     Creates a dataset to evaluate the proposed bounding boxes.
     """
 
-    def __init__(self, anno_path: str,
-                 image_path: str,
-                 proposals: List[BndBox],
-                 transform: Optional[Callable[..., Tensor]],
-                 IoU: float = 0.5) -> None:
+    def __init__(self, image_dir: str,
+                 prop_dir: str,
+                 transform: Optional[Callable[..., Tensor]]) -> None:
 
-        with open(image_path, 'rb') as f:
-            image = Image.open(f)
-            self.image = image.convert('RGB')
-        annotation = parse_xml(anno_path)
-        self.objects = [obj['bndbox'] for obj in annotation['objects']]
-        self.proposals = proposals
         self.transform = transform
-        self.IoU = IoU
+        self.item_list: List[Tuple[str, Any]] = []
+
+        for image_file in os.listdir(image_dir):
+
+            image_path = os.path.join(image_dir, image_file)
+            prop_path = os.path.join(
+                prop_dir, image_file.replace('jpg', 'csv'))
+
+            frame = pd.read_csv(prop_path)
+
+            # Crop images according to the proposals
+            for i in range(len(frame)):
+                prop = frame.iloc[i]
+                self.item_list.append((image_path, prop))
 
     def __len__(self) -> int:
 
-        return len(self.proposals)
+        return len(self.item_list)
 
     def __getitem__(self, index: int) -> Tuple[Tensor, int]:
-
-        prop = self.proposals[index]
-        image = self.image.crop((prop['xmin'], prop['ymin'],
-                                 prop['xmax'], prop['ymax']))
         
-        # If IoU > 0.5, the bounding box is marked as positive.
-        label = 0
-        for obj in self.objects:
-            if self._IoU(prop, obj) > self.IoU:
-                label = 1
-                break
+        image_path = self.item_list[index][0]
+        prop = self.item_list[index][1]
+
+        with open(image_path, 'rb') as f:
+            image = Image.open(f)
+            image = image.convert('RGB')
+        
+        image = image.crop((prop['xmin'], prop['ymin'],
+                            prop['xmax'], prop['ymax']))
+        label = prop['label']
 
         if self.transform:
             image = self.transform(image)
 
         return image, label
-    
-    @staticmethod
-    def _IoU(box1: BndBox, box2: BndBox) -> float:
-
-        x_left = max(box1['xmin'], box2['xmin'])
-        y_top = max(box1['ymin'], box2['ymin'])
-        x_right = min(box1['xmax'], box2['xmax'])
-        y_bottom = min(box1['ymax'], box2['ymax'])
-
-        if x_right < x_left or y_bottom < y_top:
-            return 0.0
-        
-        intersection_area = (x_right - x_left) * (y_bottom - y_top)
-        box1_area = (box1['xmax'] - box1['xmin']) * (box1['ymax'] - box1['ymin'])
-        box2_area = (box2['xmax'] - box2['xmin']) * (box2['ymax'] - box2['ymin'])
-        union_area = box1_area + box2_area - intersection_area
-
-        return intersection_area / union_area
